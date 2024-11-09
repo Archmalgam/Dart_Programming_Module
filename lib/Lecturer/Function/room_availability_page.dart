5779dc739824ce068142da0d1a164c72ddc7fb94
yewwen <yewwen1206@yahoo.com>
2024-11-09 14:25:47 +0800

    Added drawer and created function file

---
+++ lib/Lecturer/Function/room_availability_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RoomAvailabilityPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Manage Timetable')),
+      body: Center(child: Text('Timetable management functionality here')),
+    );
+  }
+}
--- lib/Lecturer/LecturerHomePage.dart
+++ lib/Lecturer/LecturerHomePage.dart
@@ -1,124 +1,30 @@
 import 'package:flutter/material.dart';
+import 'drawer_navigation.dart';
 
 class LecturerHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lecturer Dashboard'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Welcome, Professor!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Your Courses:',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 5, // Assuming there are 5 courses for the example
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Icon(Icons.book),
-                    title: Text('Course ${index + 1}'),
-                    subtitle: Text('Course details here...'),
-                    onTap: () {
-                      // Navigate to course details page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CourseDetailPage(courseId: index)),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Logic to view all assignments
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AssignmentsPage()),
-                );
-              },
-              child: Text('View Assignments'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Logic to manage student grades
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GradesPage()),
-                );
-              },
-              child: Text('Manage Grades'),
-            ),
-          ],
+        title: Text(
+          'Lecturer Dashboard',
+          style: TextStyle(
+            color: Color(0xFF0AC5A8), // Teal for header text
+            fontSize: 24,
+          ),
         ),
+        backgroundColor: Color(0xFF2C3C5B), // Dark Blue for AppBar background
       ),
-    );
-  }
-}
-
-// Placeholder widget for course details
-class CourseDetailPage extends StatelessWidget {
-  final int courseId;
-
-  CourseDetailPage({required this.courseId});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Course Details'),
-      ),
+      drawer: DrawerNavigation(),
       body: Center(
-        child: Text('Details for Course $courseId'),
-      ),
-    );
-  }
-}
-
-// Placeholder widget for assignments page
-class AssignmentsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Assignments'),
-      ),
-      body: Center(
-        child: Text('List of assignments goes here'),
-      ),
-    );
-  }
-}
-
-// Placeholder widget for grades management page
-class GradesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Manage Grades'),
-      ),
-      body: Center(
-        child: Text('Student grades management interface goes here'),
+        child: Text(
+          'Welcome, Professor! Select an option from the menu.',
+          style: TextStyle(
+            color: Color(0xFF5E738E), // Grey Blue for body text
+            fontSize: 18, // Slightly larger font for readability
+          ),
+          textAlign: TextAlign.center, // Center the text in the body
+        ),
       ),
     );
   }

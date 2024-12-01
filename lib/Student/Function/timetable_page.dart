de402dfdfebb7f2a50aeb2b3f24c45a58723bdd7
Dennisckk <48278765+Dennisckk@users.noreply.github.com>
2024-12-01 18:01:00 +0800

    add attendance and update timetable

--- lib/Student/Function/timetable_page.dart
+++ lib/Student/Function/timetable_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'attendance_page.dart';
 
 class TimetablePage extends StatefulWidget {
   final String studentId;
